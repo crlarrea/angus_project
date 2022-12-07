594a34f9ad65d69261bc940131724e5215d2b344
x <crlarrea@outlook.com>
2022-12-07 23:19:59 +0000

    Updates

---
+++ precache-manifest.ebaf7c8574f6902ee140057dbf40cc8c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b90e62f86a7a6b39c8a",
+    "url": "/crlarrea/angus_project/static/css/main.098ef11b.chunk.css"
+  },
+  {
+    "revision": "9b90e62f86a7a6b39c8a",
+    "url": "/crlarrea/angus_project/static/js/main.9b90e62f.chunk.js"
+  },
+  {
+    "revision": "7895cdafb8a797f1323d",
+    "url": "/crlarrea/angus_project/static/js/1.7895cdaf.chunk.js"
+  },
+  {
+    "revision": "c059734f4872dce2d22f",
+    "url": "/crlarrea/angus_project/static/js/runtime~main.c059734f.js"
+  },
+  {
+    "revision": "503e1ac87cf7e371212463729a392a23",
+    "url": "/crlarrea/angus_project/static/media/logo.503e1ac8.svg"
+  },
+  {
+    "revision": "20d41940068e45b114d32b48d684ef39",
+    "url": "/crlarrea/angus_project/static/media/Raleway-Regular.20d41940.ttf"
+  },
+  {
+    "revision": "87641f9900d717d6bfbf108b8755868e",
+    "url": "/crlarrea/angus_project/static/media/Raleway-SemiBold.87641f99.ttf"
+  },
+  {
+    "revision": "35e0e2e7a5b03275ba569a214edbab77",
+    "url": "/crlarrea/angus_project/static/media/Raleway-Black.35e0e2e7.ttf"
+  },
+  {
+    "revision": "17e4f15fd9c264d7a42545c6805bcfdf",
+    "url": "/crlarrea/angus_project/static/media/Raleway-BoldItalic.17e4f15f.ttf"
+  },
+  {
+    "revision": "d35dc615e57d1205e96c38161fcaf415",
+    "url": "/crlarrea/angus_project/index.html"
+  }
+];
