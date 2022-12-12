e464a50158cb0a4ef86b4a6eb0be152f1bd50cb5
x <crlarrea@outlook.com>
2022-12-12 17:32:06 +0000

    added pricing plans

--- src/components/Donations.js
+++ src/components/Donations.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 
+const donations = [
+  {id:'paws', amount: 2, image:''},
+  {id:'tail', amount: 5, image:''},
+  {id:'purr', amount: 10, image:''}
+]
 export const Donations = () => {
   return (
     <section className='donations'>
@@ -8,9 +16,16 @@ export const Donations = () => {
         <p>Can't adopt? Donating a small amount or sponsoring a cat can help us.</p>
       </article>
       <article>
-        <div>option 1</div>
-        <div>option 2</div>
-        <div>option 3</div>
+        {donations.map(el=>{
+          return(<div key={el.id}>
+            <img src={el.image}/>
+            <span>£{el.amount}</span>
+            <h4>{el.id} plan</h4>
+            <p>Can you spare £{el.amount} a month?<br/>Every penny helps.</p>            
+            <span><FontAwesomeIcon icon={faCirclePlus}/></span>
+          </div>)
+        })}
+        
       </article>
 
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -12,8 +12,8 @@ export const Home = () => {
           <span>Angus</span> Cat Rescue
         </h2>
         <p>
-          We are committed to a single goal: to make the world  <span>a better place
-          for cats.</span> 
+          We are committed to a single goal: to make the world{' '}
+          <span>a better place for cats.</span>
         </p>
         <p>
           We are a private, non-profit organisation dedicated to finding
@@ -28,10 +28,32 @@ export const Home = () => {
         <NavLink to='/donations'>Donate</NavLink>
       </article>
       <article>
-        <Player autoplay loop src={require('../assets/img/greeting.json')}>
+        <Player autoplay loop speed="0.2" src={require('../assets/img/cat.json')}>
           <Controls
             visible={false}
-            buttons={['play', 'repeat', 'frame', 'debug']}                     
+            buttons={['play', 'repeat', 'frame', 'debug']}
+          />
+        </Player>
+      </article>
+      <article>
+        <h3>Rehoming</h3>
+        <p>
+          We have a variety of cats looking for homes, some require
+          access to the outdoors, some are indoor only.
+        </p>
+        <p>
+          All of our cats and kittens are health-checked by our vets they have
+          at least their 1st vaccination along with microchip, and up to date
+          with flea and worm treatments.
+        </p>
+        <NavLink to='/adoption'>adopt today</NavLink>
+      </article>
+
+      <article>
+        <Player autoplay loop src={require('../assets/img/rehoming.json')}>
+          <Controls
+            visible={false}
+            buttons={['play', 'repeat', 'frame', 'debug']}
           />
         </Player>
       </article>
